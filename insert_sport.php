aa4a91007382ac9360ce75fb973e40c0fc5f55f4
premtodev08710 <64456171+premtodev08710@users.noreply.github.com>
2022-11-27 13:15:41 +0700

    แก้เพิ่มอุปกรณ์กีฬา

--- insert_sport.php
+++ insert_sport.php
@@ -6,8 +6,8 @@ $typesport_id = $_POST['typesport_id'];
 $balance = $_POST['balance'];
 $number = $_POST['number'];
 
-$sql = "INSERT INTO `sports_equipment` VALUES (NULL, '$sport_name', '$typesport_id', '$balance', '$number');";
-
+$sql = "INSERT INTO `sports_equipment` VALUES (NULL, '$sport_name', '$typesport_id', '$balance', '$number','1');";
+echo $sql;
 if ($con->query($sql) === TRUE) {
   echo "<script type='text/javascript'>";
   echo "alert('บันทึกสำเร็จ');";
@@ -16,7 +16,8 @@ if ($con->query($sql) === TRUE) {
 } else {
   echo "<script type='text/javascript'>";
   echo "alert('error เกิดข้อผิดพลาด !');";
-  echo "window.location = 'sport.php'; ";
+  echo $sql;
+  // echo "window.location = 'sport.php'; ";
   echo "</script>";
 }
 
